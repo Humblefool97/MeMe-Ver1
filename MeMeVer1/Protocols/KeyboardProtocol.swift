ec04023c71201ed44e852665dd706a148571012e
Rajeev Ranganathan <rajeevkr878@gmail.com>
2018-01-11 22:59:40 +0530

    Create a seperate folder

---
+++ MeMeVer1/Protocols/KeyboardProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  MeMeV1
+//
+//  Created by Rajeev Ranganathan on 25/12/17.
+//  Copyright © 2017 Rajeev Ranganathan. All rights reserved.
+//
+
+import Foundation
+
+protocol KeyboardProtocol {
+    func liftView(_ notification:Notification)
+   func  deLiftView(_ notification:Notification)
+}
